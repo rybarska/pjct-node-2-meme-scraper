7e40d1f9e200ea775f0cb68128e781b8d471c1a2
agata <rybarska>
2022-09-07 22:17:26 +0200

    Start working on blob and buffer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { load } from 'cheerio';
+// import blob from 'fetch-blob';
 import fetch from 'node-fetch';
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
@@ -14,3 +15,7 @@ for (let i = 0; i < 10; i++) {
   memeURLs.push($('div > a > img')[i].attribs.src);
 }
 console.log(memeURLs);
+
+/* const blob = await response.blob();
+const arrayBuffer = await blob.arrayBuffer();
+const buffer = Buffer.from(arrayBuffer);*/
