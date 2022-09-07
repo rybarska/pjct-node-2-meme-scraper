378fd3c50ff56d15492c21c0c5d9e2b5f561f2e8
agata <rybarska>
2022-09-07 15:49:27 +0200

    Add fetch and cheerio

---
+++ index.js
@@ -0,0 +1,12 @@
+import * as cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
+const url = https://memegen-link-examples-upleveled.netlify.app/;
+
+fetch(url)
+.then(function () {
+  // handle the response
+});
+.catch(function() {
+  // handle the error
+});
